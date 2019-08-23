38fcd5d44bac5b703af25c5e3e54d28744b11621
GabeButler <31711375+GabeButler@users.noreply.github.com>
2019-08-23 12:58:26 -0600

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -5,14 +5,16 @@ public class BankAccount
     private int accountNumber;
     private String password;
     private boolean loggedIn;
+    private double deposit;
     
     public BankAccount(double balance, String accountHolder, int accountNumber,
-    String password)
+    String password, double deposit)
     {
         this.balance = balance;
         this.accountHolder = accountHolder;
         this.accountNumber = accountNumber;
         this.password = password;
+        this.deposit = deposit;
         this.loggedIn = false;
     }
     
@@ -30,15 +32,28 @@ public class BankAccount
     {
         if (loggedIn)
         {
-           return balance; 
-            
+           return balance;  
+        }
+        else
+        {
+           throw new IllegalStateException("You are not logged in.");
+        }
+    }  
+    
+    public double deposit()
+    {
+        if (loggedIn)
+        {
+            double balance = this.balance + deposit;
         }
         else
         {
             throw new IllegalStateException("You are not logged in.");
         }
-    }  
+    }
+        
 
+       
     //mutator - logs user out
     public void logOut()
     {
